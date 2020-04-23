139709f8ea8504bbaf616369a349b308d13972f4
Ricardo Picinatto <ederli.picinatto@gmail.com>
2020-04-23 16:01:18 -0300

    Add the migrations table and started the post transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,36 @@
+import { getCustomRepository } from 'typeorm';
+
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category_id: category,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
